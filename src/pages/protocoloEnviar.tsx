db0d03944cd56b3d4111cd5f1b8e9de5615e464d
Mauro Imai <mtximai@gmail.com>
2022-05-17 22:54:53 -0300

    atualização

--- src/pages/protocoloEnviar.tsx
+++ src/pages/protocoloEnviar.tsx
@@ -12,11 +12,14 @@ export default function ProtocoloEnviar() {
 
   //const url = '/api/uploadDoc'
   //const url = '/api/portaljurisdicionado/processual/uploadArquivo'
+  
   //const url = 'http://localhost:2446/api/portaljurisdicionado/processual/upload'
   const url = 'http://localhost:2446/api/portaljurisdicionado/processual/uploadDoc'
 
   const [loading, setLoading] = useState(false)
   const [arquivos, setArquivos] = React.useState([])
+
+  const formRef = React.useRef()
 
   React.useEffect(() => {
     console.log('arquivos', arquivos.length)
@@ -24,9 +27,11 @@ export default function ProtocoloEnviar() {
 
 
   return (
-    <form id="form" className={styles.form} method="post" action="/api/enviarArquivo" encType="multipart/form-data">
+    <form id="form" ref={formRef} className={styles.form} method="post" action={url}>
 
       <h3>ProtocoloEnviar</h3>
+
+      <input type='text' name='msg' value='teste' />
 
       <div className={styles.fileuploaderContainer}>
         <FileUploader
@@ -48,8 +53,48 @@ export default function ProtocoloEnviar() {
         onClick={onClick}
       />
 
+      <button type="submit">
+        Submit
+      </button>
     </form>
   )
+
+  async function onClick() {
+
+    const js = {
+      msg: 'teste 345',
+      anexo: arquivos[0]
+    }
+
+
+    const fd = new FormData()
+
+    fd.append('msg', 'teste 123')
+    fd.append('anexo', arquivos[0])
+
+/*
+
+headers: {
+  'Accept': 'application/json',
+  'Content-Type': 'multipart/form-data'
+  'Content-Type': 'application/json'
+},
+
+*/
+
+    const res = await fetch (url, {
+      headers: {
+        'Accept': 'application/json',
+      },
+      method: 'POST',
+      body: fd
+    })
+
+    const r = await res.json()
+
+    console.log('resposta:', r)
+  }
+
 
   function f_onValueChange(e) {
     const files = e.value;
@@ -60,48 +105,12 @@ export default function ProtocoloEnviar() {
     //console.log('f_onValueChange', files )
   }
 
-  async function onClick() {
-    //notify('Uncomment the line to enable sending a form to the server.');
-    //formElement.current.submit();
-
-    const doc = {
-      msg: "teste",
-    }
-    //anexo: arquivos[0]
-    
-    //data.append('doc', JSON.stringify(doc))
-
-
-    const data = new FormData()
-    data.append('msg', 'teste 123')
-    data.append('anexo', arquivos[0])
-
-    const res = await fetch (url, {
-      method: 'POST',
-      body: JSON.stringify(doc)
-    })
-
-    const r = await res.json()
-
-    console.log('resposta:', r)
-  }
-
-
-
 
   // Exemplo de upload de imagem:
   const [image, setImage] = useState('')
 
-  const component = <div>
-    <input
-      type='file'
-      name='file'
-      placeholder='Upload an image'
-      onChange={uploadImage}
-    />
-  </div>
-
   const uploadImage = async e => {
+
 
     const files = e.target.files
 
@@ -120,8 +129,17 @@ export default function ProtocoloEnviar() {
 
     setImage(file.secure_url)
     setLoading(false)
-
   }
+
+  const component = <div>
+    <input
+      type='file'
+      name='file'
+      placeholder='Upload an image'
+      onChange={uploadImage}
+    />
+  </div>
+
 
 }
 
