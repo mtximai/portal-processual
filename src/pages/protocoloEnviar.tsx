11a79b5a62780a582f9c3a85f98a8947eae9a21e
Mauro Imai <mtximai@gmail.com>
2022-05-23 17:49:21 -0300

    atualização

--- src/pages/protocoloEnviar.tsx
+++ src/pages/protocoloEnviar.tsx
@@ -129,19 +129,17 @@ export default function ProtocoloEnviar() {
   const [image, setImage] = useState('')
 
   const uploadImage = async e => {
-
-
     const files = e.target.files
 
-    const data = new FormData()
-    data.append('file', files[0])
-    data.append('upload_preset', 'darwin')
+    const fd = new FormData()
+    fd.append('file', files[0])
+    fd.append('upload_preset', 'darwin')
 
     setLoading(true)
 
     const res = await fetch ('url', {
       method: 'POST',
-      body: data
+      body: fd
     })
 
     const file = await res.json()
@@ -160,4 +158,3 @@ export default function ProtocoloEnviar() {
   </div>
 
 }
-
